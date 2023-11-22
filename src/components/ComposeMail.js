3c64bd489c3cf8c0e8d174fb64c5adbb5badec93
Gintama7 <43084982+Gintama7@users.noreply.github.com>
2023-11-22 10:53:52 +0530

    fixed text editor

--- src/components/ComposeMail.js
+++ src/components/ComposeMail.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { Button, Card, Container, Form } from 'react-bootstrap';
-import { Editor } from "react-draft-wysiwyg";
+// import { Editor } from "react-draft-wysiwyg";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import { mailActions } from '../store/mail-slice';
+import JoditEditor, { Jodit } from 'jodit-react';
 
 const ComposeMail = () => {
     const [usermail,setUserMail] = useState('');
@@ -14,12 +15,13 @@ const ComposeMail = () => {
     const loggedMail= useSelector(state=> state.auth.email);
     const history = useHistory();
     const dispatch = useDispatch();
-    const unreadIn= useSelector(state=>state.mail.unreadInbox);
-    const unreadOut= useSelector(state=>state.mail.unreadSent);
+    // const editor = useRef(null);
 
     const sendHandler=async(e)=>{
        
 e.preventDefault();
+const plaintext = Jodit.modules.Helpers.stripTags(mailBody);
+
     
         let sendingMail ='';
         for(let i=0;i<usermail.length;i++)
@@ -31,14 +33,14 @@ e.preventDefault();
         }
         try{
            const res = await axios.post(`https://mail-box-client-39877-default-rtdb.firebaseio.com/emails/${sendingMail}/incoming.json`,
-        {id:subject,from:loggedMail,subject:subject,message:mailBody,read:false});
+        {id:subject,from:loggedMail,subject:subject,message:plaintext,read:false});
 
         
 
         console.log('email sent');
         axios.post(`https://mail-box-client-39877-default-rtdb.firebaseio.com/emails/${loggedMail}/sent.json`,
-        {id:subject,to:usermail,subject:subject,message:mailBody,read:false});
-        dispatch(mailActions.addMailToSent({id:subject,to:usermail,subject:subject,message:mailBody,read:false}));
+        {id:subject,to:usermail,subject:subject,message:plaintext,read:false});
+        // dispatch(mailActions.addMailToSent({id:subject,to:usermail,subject:subject,message:plaintext,read:false}));
         }catch(err)
         {
             alert(err);
@@ -48,6 +50,11 @@ e.preventDefault();
         setSubject('');
         setMailBody('');
         history.replace('/home');
+    }
+
+    const changeHandler =(value)=>{
+        setMailBody(value);
+        
     }
 
   return (
@@ -61,21 +68,16 @@ e.preventDefault();
         <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Control type="email" placeholder="Subject" onChange={(e)=>setSubject(e.target.value)} />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1" onChange={(e)=>setMailBody(e.target.value)}>
-        {/* <Form.Label>Mail Body</Form.Label> */}
-        <Form.Control as="textarea" rows={3}  />
-      </Form.Group>
+        {/* <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1" onChange={(e)=>setMailBody(e.target.value)}>
+       
+        <Form.Control as="textarea" rows={3} value={mailBody}  />
+      </Form.Group> */}
+      <JoditEditor id="my-editor" value={mailBody} onChange={changeHandler}/>
     </Form>
    </Card.Body>
 <Card.Footer className='d-flex align-items-center justify-content-center m-0'>
     <Button onClick={sendHandler}>Send</Button>
-<Editor
-//   editorState={editorState}
-  toolbarClassName="toolbarClassName"
-  wrapperClassName="wrapperClassName"
-  editorClassName="editorClassName"
-//   onEditorStateChange={this.onEditorStateChange}
-/>
+
 </Card.Footer>
 </Card>
    </Container>
--- src/components/SiteNavbar.js
+++ src/components/SiteNavbar.js
@@ -18,12 +18,11 @@ const SiteNavbar = () => {
   return (
     <Navbar expand="lg" className="bg-body-tertiary" >
     <Container>
-      <Navbar.Brand href="/">MyWebLink</Navbar.Brand>
+      <Navbar.Brand href="/">Mail Client</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
           <Nav.Link href="/home">Home</Nav.Link>
-          <Nav.Link href="#products">Products</Nav.Link>
           <Nav.Link href="#link">About Us</Nav.Link>
         </Nav>
        {isLoggedIn && <Nav>
